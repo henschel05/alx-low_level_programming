05ac176cf362000b1370c285b0d543e151d0bfa2
henschel05 <henschelzulu05@gmail.com>
2021-08-29 13:35:29 -0700

    intil commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
-/*
- * Text text text
- * Main - prints answer
- * Return : 0
+
+/**
+ * main - prints out the alphabet in lowercase except for 'qe'
+ * Return: 0
  */
 int main(void)
 {
+
   char c;
 
   for (c = 'a'; c <= 'z'; c++)
-    {
-      if (c != 'e' && c != 'q')
-	{
-	  putchar(c);
-	}
-    }
-
+    if (c != 'e' && c != 'q')
+      putchar(c);
   putchar('\n');
-
   return (0);
 }
