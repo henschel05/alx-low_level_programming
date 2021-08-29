37fe93e679dd135af785d42b52160ee0b2fd4763
henschel05 <henschelzulu05@gmail.com>
2021-08-29 16:51:31 -0700

    betty fixing

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,21 +1,18 @@
-#include <stdio.h>
-
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints to string
+ * Description: prints alphabet in lowercase
+ * Return: 0
  */
+#include <stdio.h>
 int main(void)
 {
-  char l;
-
-  l = 'z';
-  while (l >= 'a')
-    {
-      putchar(l);
-      l--;
-    }
-  putchar('\n');
-  return (0);
+char l;
+l = 'z';
+while (l >= 'a')
+{
+putchar(l);
+l--;
+}
+putchar('\n');
+return (0);
 }
