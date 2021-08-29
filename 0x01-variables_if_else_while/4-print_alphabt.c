660a7b932a7195792d6cf6d5e33efdfc46c6e78f
henschel05 <henschelzulu05@gmail.com>
2021-08-29 14:31:26 -0700

    intil commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,46 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description: Get a random number and check its last digit, compare it with 5
- * Return: 0
+ * main - Prints 3 combination of numbers
+ *
+ * Return: Always (Success)
  */
 int main(void)
 {
-char c = 'a';
-while (c <= 'z')
-{
-if (c != 'e' && c != 'q')
-{
- putchar(c);
-}
-c++;
-}
+  int c, i, k, j;
 
-putchar('\n');
-return (0);
+  for (c = 48; c <= 57; c++)
+    {
+      for (i = 48; i <= 57; i++)
+	{
+	  for (k = 48; k <= 57; k++)
+	    {
+	      for (j = 48; j <= 57; j++)
+		{
+		  if (((k + j) > (c + i) &&  k >= c) || c < k)
+		    {
+		      putchar(c);
+		      putchar(i);
+		      putchar(' ');
+		      putchar(k);
+		      putchar(j);
+
+		      if (c + i + k + j == 227 && c == 57)
+			{
+			  break;
+			}
+		      else
+			{
+			  putchar(',');
+			  putchar(' ');
+			}
+		    }
+		}
+	    }
+	}
+    }
+
+  putchar('\n');
+
+  return (0);
 }
