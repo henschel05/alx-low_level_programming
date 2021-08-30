26d849d72e8e7c67328c9e4d49fcfec78359570c
henschel05 <henschelzulu05@gmail.com>
2021-08-29 17:27:45 -0700

    betty fixing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
@@ -10,37 +8,34 @@
  */
 int main(void)
 {
-  int i;
-  int j;
-
-  i = 48;
-  j = 49;
-  while  ((i < 57) && (j < 58))
-    {
-      putchar(i);
-      putchar(j);
-      if ((i == 56) && (j == 57))
-	{
-	  putchar('\n');
-	  i++;
-	  j++;
-	}
-      else
-	{
-	  putchar(44);
-	  putchar(32);
-	  if (j < 57)
-	    {
-	      j++;
-	    }
-	  else
-	    {
-	      i++;
-	      j = 1 + i;
-	    }
-	}
-
-    }
-
-  return (0);
+int i;
+int j;
+i = 48;
+j = 49;
+while  ((i < 57) && (j < 58))
+{
+putchar(i);
+putchar(j);
+if ((i == 56) && (j == 57))
+{
+putchar('\n');
+i++;
+j++;
+}
+else
+{
+putchar(44);
+putchar(32);
+if (j < 57)
+{
+j++;
+}
+else
+{
+i++;
+j = 1 + i;
+}
+}
+}
+return (0);
 }
