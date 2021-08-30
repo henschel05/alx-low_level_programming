b33f965af8b312427b4340b0d4117433a51a7632
henschel05 <henschelzulu05@gmail.com>
2021-08-29 17:10:19 -0700

    betty fixing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,33 +1,29 @@
+/**
+ * main - prints to string
+ *Description - prints alphabet in lowercase
+ *Return 0
+ */
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/* betty style doc for function main goes there */
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-  int i;
-
-  i = 48;
-  while  (i < 58)
-    {
-      putchar(i);
-      if (i !=  57)
-	{
-	  putchar(44);
-	  putchar(32);
-	  i++;
-	}
-      else
-	{
-	  i++;
-	}
-    }
-  putchar('\n');
-
-  return (0);
+int i;
+i = 48;
+while  (i < 58)
+{
+putchar(i);
+if (i !=  57)
+{
+putchar(44);
+putchar(32);
+i++;
+}
+else
+{
+c++;
+}
+}
+putchar('\n');
+return (0);
 }
