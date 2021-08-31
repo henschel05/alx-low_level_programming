2c3a5f400a152992b6d344da3232cb37370eab6d
henschel05 <henschelzulu05@gmail.com>
2021-08-31 08:23:27 -0700

    intil commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,15 +8,14 @@
  */
 int main(void)
 {
-  int i = 0;
-  char c;
-  char s[] = "Holberton\n";
-
-  while (i <= 9)
-    {
-      c = s[i];
-      _putchar(c);
-      i++;
-    }
-  return (0);
+int i = 0;
+char c;
+char s[] = "Holberton\n";
+while (i <= 9)
+{
+c = s[i];
+_putchar(c);
+i++;
+}
+return (0);
 }
