ba739f7e27c66bbc4fd32ddb4e64632eabf83525
henschel05 <henschelzulu05@gmail.com>
2021-08-31 12:45:29 -0700

    intil commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,20 @@
 #include "holberton.h"
 /**
- * main - entry point
+ * main - Entry point
  *
- * Description: Prints Holberton with _putchar
- *
- * Return: always 0
+ * Return: Always 0
  */
 int main(void)
 {
-int i = 0;
-char c;
-char s[] = "Holberton\n";
-while (i <= 9)
-{
-c = s[i];
-_putchar(c);
-i++;
-}
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
 }
