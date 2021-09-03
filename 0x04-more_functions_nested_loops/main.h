10b0313c5dff6d912e2f401ec071c7d6b181a095
henschel05 <henschelzulu05@gmail.com>
2021-09-03 07:14:55 -0700

    intil commit

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+/**
+ * _isupper - Entry point
+ * Description: prints alphabet
+ *@c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+#include "main.h"
+#include <stdio.h>
+int _isupper(int c)
+{
+  int i = 'A';
+  for (i = 'A'; i <= 'Z'; i++)
+    {
+      /* refer int c*/
+      if (c == i)
+	{
+	  return (1);
+	}
+    }
+  return (0);
+}
