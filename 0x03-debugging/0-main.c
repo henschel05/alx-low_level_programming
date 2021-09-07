7fdbe677c005b717c7fe354bc8e48ffe11331403
henschel05 <henschelzulu05@gmail.com>
2021-09-06 21:05:03 -0700

    intil commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,31 +1,12 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
-/* betty style doc for function main goes there */
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+  * main - Test function for positive or negative
+  * Return: 0
+  */
 int main(void)
-{	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%i is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%i is zero\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%i is negative\n", n);
-	}
-
-	return (0);
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
