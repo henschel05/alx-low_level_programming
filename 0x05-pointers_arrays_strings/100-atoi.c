17e675c3ef0c7c093938472d38e238d5eef305fb
henschel05 <henschelzulu05@gmail.com>
2021-09-06 22:35:34 -0700

    intil commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,25 +1,33 @@
 #include "holberton.h"
 /**
- * _atoi -converts a string to an integer
- * @s: string to be converted
- * Return: value of integer
+ * _atoi - function that convert a string to an integer
+ * @s: string to convert
+ * Return: int
  */
 int _atoi(char *s)
 {
-int i, j, n, m;
-i = n = 0;
-m = 1;
-while ((*(s + i) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
+int i = 0;
+int n = 0;
+int signo = 1;
+while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 {
-if (*(s + i) == '-')
-m *= -1;
+if (s[i] == '-')
+signo *= -1;
 i++;
 }
-j = i;
-while ((*(s + j) >= '0') && (*(s + j) <= '9'))
+while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
 {
-n = n * 10 + m * (*(s + j) - '0');
-j++;
+if (n >= 0)
+{
+n = n * 10 - (s[i] - '0');
+i++;
 }
-return (n);
+else
+{
+n = n * 10 - (s[i] - '0');
+i++;
+}
+}
+signo *= -1;
+return (n * signo);
 }
